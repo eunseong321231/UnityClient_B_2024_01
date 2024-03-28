5523ae637867bf730db0266aa02df0178bdb3da3
eunseong <csx1245@gmail.com>
2024-03-28 11:20:19 +0900

    Feat : 저장 암호화 까지
    '24.03.28. 목

---
+++ UnityClient_B_2024_01/Assets/Scripts/Character/ExCharacterDifferent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExCharacterDifferent : ExCharacter
+{
+    // Start is called before the first frame update
+
+
+    protected override void Move()
+        {
+            base.Move();
+            transform.Translate(
+                Vector3.up * speed * Time.deltaTime);
+        }
+ }
+
